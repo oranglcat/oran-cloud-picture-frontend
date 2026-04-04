c350bc90e5c696e0f2b657ef30be6c2ba0d9293f
oranglcat <2496503479@qq.com>
2026-04-04 22:20:32 +0800

    图片管理页面开发 图片创建页面开发 主页开发

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
-  <div id = "app">
+  <div id="app">
     <BasicLayout />
   </div>
 </template>
 
 <script setup lang="ts">
-import BasicLayout from "@/layouts/BasicLayout.vue";
-import {healthUsingGet} from "@/api/mainController.ts";
-import {useLoginUserStore} from "@/stores/useLoginUserStore.ts";
+import BasicLayout from '@/layouts/BasicLayout.vue'
+// import { healthUsingGet } from '@/api/mainController.ts'
+// import {useLoginUserStore} from "@/stores/useLoginUserStore.ts";
 
-const LoginUserStore = useLoginUserStore();
-LoginUserStore.fetchLoginUser();
+// 已经改为在权限校验文件中获取
+// const loginUserStore = useLoginUserStore()
+// loginUserStore.fetchLoginUser()
 
-healthUsingGet().then(res => {
-  console.log(res);
-})
+// healthUsingGet().then((res) => {
+//   console.log(res)
+// })
 </script>
-<style>
-</style>
+
+<style scoped></style>
--- src/stores/useLoginUserStore.ts
+++ src/stores/useLoginUserStore.ts
@@ -1,35 +1,37 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import { getLoginUserUsingGet } from '@/api/userController.ts'
 
-/*
+/**
  * 存储登录用户信息的状态
- * */
+ */
 export const useLoginUserStore = defineStore('loginUser', () => {
-  //定义变量的初始值
-  const loginUser = ref<any>({
+  const loginUser = ref<API.LoginUserVO>({
     userName: '未登录',
   })
 
-
-  /*
-  * 获取登录用户
-  * */
+  /**
+   * 远程获取登录用户信息
+   */
   async function fetchLoginUser() {
-    //todo 后端接口还未开发，暂时为空
-    setTimeout(() => {
-      loginUser.value = {
-        userName: '测试用户',
-        id: 1
-      }
-    },3000)
+    const res = await getLoginUserUsingGet()
+    if (res.data.code === 0 && res.data.data) {
+      loginUser.value = res.data.data
+    }
+    // // 测试用户登录，3 秒后自动登录
+    // setTimeout(() => {
+    //   loginUser.value = { userName: '测试用户', id: 1 }
+    // }, 3000)
   }
 
-  /*
+  /**
    * 设置登录用户
-   * */
-  function setLoginUser(user: any): void {
-    loginUser.value = user
+   * @param newLoginUser
+   */
+  function setLoginUser(newLoginUser: any) {
+    loginUser.value = newLoginUser
   }
-  //返回存储的状态和计算逻辑
-  return {loginUser, fetchLoginUser, setLoginUser}
+
+  // 返回
+  return { loginUser, fetchLoginUser, setLoginUser }
 })
